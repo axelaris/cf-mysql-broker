c3f5ba033820c19d55ee620701fb19894498688c
Alexey Zakharov <alexey.zakharov@altoros.com>
2018-04-28 17:59:06 +0300

    Added environment variables for host and password

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sed -i.bak "s/VAR_HOST/$VAR_HOST/" config/database.yml
+sed -i.bak "s/VAR_PASS/$VAR_PASS/" config/database.yml
 bundle exec rake db:setup
 rails server -b 0.0.0.0 -p 8080
